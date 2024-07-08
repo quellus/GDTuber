64964b423cc01a2738187e085ae1441b7895eae0
SpaghettiCodeFactory <spaghetticodefactory@gmail.com>
2024-07-09 00:07:36 +0200

    refactor amp, add compressor for smoother output

--- scripts/MicRecord.gd
+++ scripts/MicRecord.gd
@@ -5,11 +5,18 @@ var bus_index
 var analyzer: AudioEffectSpectrumAnalyzerInstance
 var samples: Array[float] = []
 
-var amplify_effect : AudioEffectAmplify
-var default_input_gain : float = 0.0
+var amplifier_effect : AudioEffectAmplify
+var amplifier_input_gain : float = 0.0
+
+var compressor_effect : AudioEffectCompressor
+var compressor_threshold : float = -12.0 # Compression threshold in dB
+var compressor_ratio : float = 2.0 # Compression ratio in dB
+var compressor_attack_time : float = 5000.0 # Attack time in MICROseconds
+var compressor_release_time : float = 300.0 # Release time in MILLIseconds
+var compressor_makeup_gain : float = 9.0 # Makeup gain that get's add to the compressor output signal
 
 const SCALE_RATIO = 1.1
-const MAX_SAMPLES = 10
+const MAX_SAMPLES = 20
 
 @onready var menu = %Menu
 @onready var input_gain_slider = %InputGainSlider
@@ -26,11 +33,15 @@ func _ready():
 	get_tree().get_root().set_transparent_background(true)
 	bus_index = AudioServer.get_bus_index("Record")
 	
-	init_amplifier(bus_index,0.0)
+	init_amplifier(bus_index,amplifier_input_gain)
+	init_compressor(bus_index)
+	
 	input_gain_slider.connect("value_changed",_on_input_gain_change)
 
 func _process(_delta):
-	var magnitude = db_to_linear(AudioServer.get_bus_peak_volume_left_db(bus_index, 0))
+	var current_db = AudioServer.get_bus_peak_volume_left_db(bus_index, 0)
+	var magnitude = db_to_linear(current_db)
+	$Menu/PanelContainer/VBoxContainer/CurrentGainLabel.text = str(snapped(current_db,1.0))
 	
 	if samples.size() >= MAX_SAMPLES:
 		samples.pop_front()
@@ -55,22 +66,35 @@ func _unhandled_input(event):
 
 func _get_average() -> float:
 	var mag_sum: float = 0.0
+	var mag_avg: float = 0.0
 	for i in samples:
 		mag_sum += i
-	return mag_sum / float(samples.size())
+	mag_avg = mag_sum / float(samples.size())
+	#$Menu/PanelContainer/VBoxContainer/CurrentGainLabel.text = str(mag_avg)
+	return mag_avg
 	
 	
 func init_amplifier(_bus_index : int, _input_gain : float = 0.0):
-	amplify_effect = AudioEffectAmplify.new()
-	amplify_effect.volume_db = _input_gain
-	AudioServer.add_bus_effect(_bus_index,amplify_effect,1)
+	amplifier_effect = AudioEffectAmplify.new()
+	amplifier_effect.volume_db = _input_gain
+	AudioServer.add_bus_effect(_bus_index,amplifier_effect,1)
+	print("Amplifier effect initialized")
+	
+func init_compressor(_bus_index : int):
+	compressor_effect = AudioEffectCompressor.new()
+	compressor_effect.threshold = compressor_threshold
+	compressor_effect.attack_us = compressor_attack_time
+	compressor_effect.release_ms = compressor_release_time
+	compressor_effect.gain = compressor_makeup_gain
+	AudioServer.add_bus_effect(_bus_index,compressor_effect,2)
+	print("Compressor effect initialized")
 	
 func update_amplifier(_new_input_gain : float):
-	if _new_input_gain <= -10.0 || _new_input_gain >= 20.0:
+	if _new_input_gain <= -10.0 || _new_input_gain >= 24.1:
 		printt("Input gain out of range:",_new_input_gain)
 		return
-	if amplify_effect.volume_db != _new_input_gain:
-		amplify_effect.volume_db = _new_input_gain
+	if amplifier_effect.volume_db != _new_input_gain:
+		amplifier_effect.volume_db = _new_input_gain
 		printt("set amplify gain to:",_new_input_gain)
 
 
