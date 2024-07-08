6e9e1c42a7c24a0dcd3ecf5cacdea103d231cc1c
SpaghettiCodeFactory <spaghetticodefactory@gmail.com>
2024-07-09 00:16:55 +0200

    start menu code cleanup

--- MainMenu.gd
+++ MainMenu.gd
@@ -5,11 +5,13 @@ var default_window_size : Vector2 = Vector2(1280,800)
 
 @onready var run_window_button = $CenterContainer/VBoxContainer2/VBoxContainer/RunWindowButton
 @onready var run_fullscreen_button = $CenterContainer/VBoxContainer2/VBoxContainer/RunFullscreenButton
+@onready var quit_button = $CenterContainer/VBoxContainer2/VBoxContainer/QuitButton
 
 
 func _ready():
 	run_window_button.connect("pressed",run_windowed)
 	run_fullscreen_button.connect("pressed",run_fullscreen)
+	quit_button.connect("pressed",quit_app)
 
 func run_app():
 	get_tree().change_scene_to_file("res://scenes/MicRecord.tscn")
@@ -24,7 +26,7 @@ func run_fullscreen():
 	toggle_fullscreen()
 	run_app()
 	
-func quit():
+func quit_app():
 	get_tree().quit()
 	
 func change_window_size(size: Vector2):
