42c405acad561e865a0ca0610b855edb094c51e0
quellus <quellus7@gmail.com>
2025-02-01 14:25:07 -0700

    Fixed missing margins on Animation Tab

--- scripts/Settings Menu.gd
+++ scripts/Settings Menu.gd
@@ -46,74 +46,74 @@ signal blink_duration_change(blink_duration)
 @onready var blinkdurationsettingsdisplay = %BlinkDurationSettingsInput
 
 func _ready():
-    %BlinkMinIntervalIncreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMinIntervalIncreaseButton))
-    %BlinkMinIntervalDecreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMinIntervalDecreaseButton))
-    %BlinkMaxIntervalIncreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMaxIntervalIncreaseButton))
-    %BlinkMaxIntervalDecreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMaxIntervalDecreaseButton))
+	%BlinkMinIntervalIncreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMinIntervalIncreaseButton))
+	%BlinkMinIntervalDecreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMinIntervalDecreaseButton))
+	%BlinkMaxIntervalIncreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMaxIntervalIncreaseButton))
+	%BlinkMaxIntervalDecreaseButton.pressed.connect(func(): update_blink_intervals(%BlinkMaxIntervalDecreaseButton))
 
-    %BlinkDurationIncreaseButton.pressed.connect(func(): update_blink_duration(%BlinkDurationIncreaseButton))
-    %BlinkDurationDecreaseButton.pressed.connect(func(): update_blink_duration(%BlinkDurationDecreaseButton))
+	%BlinkDurationIncreaseButton.pressed.connect(func(): update_blink_duration(%BlinkDurationIncreaseButton))
+	%BlinkDurationDecreaseButton.pressed.connect(func(): update_blink_duration(%BlinkDurationDecreaseButton))
 
 func update_blink_duration(blinkdurationbutton: TextureButton):
-    var min_duration = 1 
-    var duration = int(blinkdurationsettingsdisplay.text)
+	var min_duration = 1 
+	var duration = int(blinkdurationsettingsdisplay.text)
 
-    match blinkdurationbutton.name:
-        "BlinkDurationIncreaseButton":
-            duration+=1 
-        "BlinkDurationDecreaseButton":
-            duration-=1 
+	match blinkdurationbutton.name:
+		"BlinkDurationIncreaseButton":
+			duration+=1 
+		"BlinkDurationDecreaseButton":
+			duration-=1 
 
-    if duration < min_duration: 
-        duration=min_duration
-    
-    blinkdurationsettingsdisplay.text = str(duration)
-    blink_duration_change.emit(duration)
+	if duration < min_duration: 
+		duration=min_duration
+	
+	blinkdurationsettingsdisplay.text = str(duration)
+	blink_duration_change.emit(duration)
 
 
 func update_blink_intervals(blinkintervalbutton: TextureButton):
-    var min_int = int(minintervalsettingsdisplay.text)
-    var max_int = int(maxintervalsettingsdisplay.text)
+	var min_int = int(minintervalsettingsdisplay.text)
+	var max_int = int(maxintervalsettingsdisplay.text)
 
-    var min_first=true
-    
-    match blinkintervalbutton.name:
-        "BlinkMinIntervalIncreaseButton":
-            min_int+=1 
-        "BlinkMinIntervalDecreaseButton":
-            min_int-=1 
-        "BlinkMaxIntervalIncreaseButton":
-            min_first=false
-            max_int+=1 
-        "BlinkMaxIntervalDecreaseButton":
-            min_first=false
-            max_int-=1 
+	var min_first=true
+	
+	match blinkintervalbutton.name:
+		"BlinkMinIntervalIncreaseButton":
+			min_int+=1 
+		"BlinkMinIntervalDecreaseButton":
+			min_int-=1 
+		"BlinkMaxIntervalIncreaseButton":
+			min_first=false
+			max_int+=1 
+		"BlinkMaxIntervalDecreaseButton":
+			min_first=false
+			max_int-=1 
 
-    var validated_min_max = min_max_validate(min_int, max_int, min_first)
+	var validated_min_max = min_max_validate(min_int, max_int, min_first)
 
-    minintervalsettingsdisplay.text = str(validated_min_max[0])
-    maxintervalsettingsdisplay.text = str(validated_min_max[1])
+	minintervalsettingsdisplay.text = str(validated_min_max[0])
+	maxintervalsettingsdisplay.text = str(validated_min_max[1])
 
-    blink_speed_change.emit(validated_min_max[0], validated_min_max[1])
+	blink_speed_change.emit(validated_min_max[0], validated_min_max[1])
 
 
 func min_max_validate(min_blink_interval: int, max_blink_interval: int, min_first: bool):
-    var max_min = 2
-    var min_min = 1
-    var min_blink: int = abs(min_blink_interval)
-    var max_blink: int = abs(max_blink_interval)
-    
-    min_blink = min_min if  min_blink == 0 else min_blink
-    max_blink = max_min if  max_blink == 1 else max_blink
+	var max_min = 2
+	var min_min = 1
+	var min_blink: int = abs(min_blink_interval)
+	var max_blink: int = abs(max_blink_interval)
+	
+	min_blink = min_min if  min_blink == 0 else min_blink
+	max_blink = max_min if  max_blink == 1 else max_blink
 
-    if min_first:
-        if min_blink >= max_blink: 
-            max_blink = min_blink+1
-    else:
-        if max_blink <= min_blink: 
-            min_blink = max_blink-1
+	if min_first:
+		if min_blink >= max_blink: 
+			max_blink = min_blink+1
+	else:
+		if max_blink <= min_blink: 
+			min_blink = max_blink-1
 
-    return [min_blink, max_blink]
+	return [min_blink, max_blink]
 
 
 
