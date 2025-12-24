b7a0eadbebd091a892551501ee19fd9d74a00c35
Gustavo Lopes <gustavolr035@gmail.com>
2025-12-24 19:41:52 -0300

    Add Brazilian Portuguese translation support, and Translation fixes
    - The function that changes translation to default now checks both the OS language and full locale against available_locales, setting the locale accordingly. This enhances localization accuracy by supporting more specific locale matches.
    - Add Brazilian Portuguese translation support, and Translation fixes
    - Added Brazilian Portuguese CSV translation file and import configuration.
    - Updated project settings and localization script to support the new language.
    - Changed Panel(s) to PanelContainer for better text visibility.
    - Fixed logic in the translated texts so "Current FPS" and "Default" text is updated correctly
    - Fixed bug that didn't applied correctly changes from one language to "Default"

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -65,6 +65,7 @@ var input_device: String
 @onready var max_fps_spinbox = %MaxFPSSpinbox
 @onready var fps_cap: bool = false
 @onready var fps_cap_value: int = 0
+var FPS_LABEL_SETTING : String = ""
 
 # Audio Management
 @onready var input_gain_slider: Slider = %InputGainSlider
@@ -80,6 +81,9 @@ var input_device: String
 func _ready():
 	# Set Window Properties
 	get_tree().get_root().set_transparent_background(true)
+
+	# Set FPS translation label for later translations
+	FPS_LABEL_SETTING = %CurrentFPSLabel.text
 
 	# Set Audio Properties
 	bus_index = AudioServer.get_bus_index("Record")
@@ -122,8 +126,8 @@ func _process(_delta):
 			is_talking = false
 
 	%VolumeVisual.value = magnitude_avg
-
-	%CurrentFPSLabel.set_text("Current FPS %.1f" % Engine.get_frames_per_second())
+	
+	%CurrentFPSLabel.set_text("%s %.1f" % [ tr(FPS_LABEL_SETTING), Engine.get_frames_per_second()])
 
 
 func _notification(what):
